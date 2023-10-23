c2f32a8edd33e11c3cc8dd4b37aee2ea1b764480
DevWeb13 <giuliano.loic@sfr.fr>
2023-10-23 16:28:05 +0200

    test: Test SonarCloud

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -50,7 +50,7 @@ export async function POST(request: any): Promise<NextResponse> {
 
     const mailOption: SendMailOptions = {
       from: `${email} <${process.env.EMAIL}>`,
-      to: process.env.EMAIL, //Changer pour carrosse-pat@hotmail.fr au moment de la mise en ligne
+      to: process.env.EMAIL, //Changer pour carrosse-pat@hotmail.fr au moment de la mise en ligne.
       replyTo: email,
       subject: `Message from ${name} - ${phone} - ${email}`,
       html: `<p>${message}</p><br><p>Photos:</p><br>${photoLinks}`,
